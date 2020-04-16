cd881077fcc6372efdc17e8fcb9308796935fc78
Jessica Nahulan <JessicaN@ibm.com>
2020-04-16 00:55:43 -0400

    Made base application structure and added some graphics

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import Test from "./Test";
-import HeaderBar from "./HeaderBar/HeaderBar";
-import { Button } from 'carbon-components-react';
+import { HashRouter } from "react-router-dom";
 import './App.scss';
+import CommunityBridge from "./CommunityBridge";
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <HeaderBar/>
-      </header>   
-      <div>
-        <div className={"HeaderGradient"}>
-          <Button>Hello React!</Button>
-          <Test/>
-        </div>
-        <svg className="curve-container" viewBox="0 0 1440 68" enableBackground="new 0 0 1440 68">
-          <path d="m1622.3 1937.7c0 0-410.7 169.1-913.4 75.5-502.7-93.6-977.7 56.3-977.7 56.3v440h1891.1v-571.8" fill="#fff" transform="translate(0-1977)"></path>
-        </svg>
-      </div> 
-    </div>
-  );
+	return (
+		<div className="App">
+			<HashRouter>
+				<CommunityBridge/>
+			</HashRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Welcome/Welcome.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './Welcome.scss';
+import {ReactComponent as ReactSvg} from '../assets/girlAndSenior.svg';
+import {ReactComponent as ReactSvgFam} from '../assets/stayHomeFamily.svg';
+import withAnimationEaseIn from "../Utilities/withAnimationEaseIn/withAnimationEaseIn.js";
+import withAnimationEaseOut from "../Utilities/withAnimationEaseOut/withAnimationEaseOut.js";
+
+class Welcome extends Component {
+	state = {renderedImg1: false,  unmountProcedureActive: false}
+	Timer;
+
+	componentDidMount() {
+		this.Timer = setTimeout(() => {
+			this.setState({unmountProcedureActive: true});
+		}, 5000);
+		this.Timer = setTimeout(() => {
+			this.setState({renderedImg1: true, unmountProcedureActive: false});
+		}, 6500);
+	}
+
+	componentWillUnmount() {
+		clearTimeout(this.Timer);
+	}
+
+	render(){
+		const Img1 = this.state.unmountProcedureActive ? withAnimationEaseOut(ReactSvgFam) : withAnimationEaseIn(ReactSvgFam);
+		const Img2 = this.state.unmountProcedureActive ? withAnimationEaseOut(ReactSvg) : withAnimationEaseIn(ReactSvg);
+		return (
+			<React.Fragment>
+				<div className={"HeaderGradient"}>
+					We may be apart, but we'll get through this together.
+				</div>
+				<div>
+					{ !this.state.renderedImg1 ?
+						<div className="Phone_Graphic_Handle"><Img1/></div>: 
+						<div className="Phone_Graphic_Handle"><Img2/></div>
+					}
+					
+				</div>
+			</React.Fragment>
+		);
+	}
+
+}
+
+export default Welcome;
